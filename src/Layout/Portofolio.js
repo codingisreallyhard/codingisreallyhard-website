da7e00af4fff51e4610faa2a14c11b41df4875f3
codingisreallyhard <florinconstantin99@yahoo.com>
2022-08-30 23:35:14 +0300

    added portofolio img code

--- src/Layout/Portofolio.js
+++ src/Layout/Portofolio.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Portofolio.css";
-import authapp from "../Assets/authapp.png";
-import guess from "../Assets/guessgame.png";
-import eshop from "../Assets/reacteshop.png";
+
+import PortofolioImage from "./PortofolioImage";
 function Portofolio() {
   return (
     <section id="my-works">
@@ -10,66 +9,13 @@ function Portofolio() {
         <div className="proj-heading">
           <h1>Portfolio</h1>
         </div>
-        <div className="portfolio-content container">
-          <div className="proj-1">
-            <img src={eshop} alt="React Eshop project" />
-            <div className="proj1-details">
-              <i className="fab fa-html5"></i>
-              <i className="fab fa-css3-alt"></i>
-              <i className="fab fa-js"></i>
-              <h2>React E-Shop</h2>
-              <p>Built Using HTML,CSS,React.js</p>
-              <button>
-                <a
-                  href="https://github.com/codingisreallyhard/React-E-Shop"
-                  target="blank"
-                >
-                  View source <i className="fas fa-external-link-alt"></i>
-                </a>
-              </button>
-            </div>
-          </div>
 
-          <div className="proj-2">
-            <img src={authapp} alt="React Auth" />
-            <div className="proj2-details">
-              <i className="fab fa-html5"></i>
-              <i className="fab fa-css3-alt"></i>
-              <h2>Small Autenthication App</h2>
-              <p>Built Using HTML,CSS, React.js</p>
-              <button>
-                <a
-                  href="https://github.com/codingisreallyhard/React-Auth-Project"
-                  target="blank"
-                >
-                  View source <i className="fas fa-external-link-alt"></i>
-                </a>
-              </button>
-            </div>
-          </div>
-          <div className="proj-2">
-            <img src={guess} alt="Guess game" />
-            <div className="proj2-details">
-              <i className="fab fa-html5"></i>
-              <i className="fab fa-css3-alt"></i>
-              <h2>Guess Game</h2>
-              <p>Built Using HTML,CSS, JAVASCRIPT</p>
-              <button>
-                <a
-                  href="https://github.com/codingisreallyhard/Guess-Game-Javascript"
-                  target="blank"
-                >
-                  View source <i className="fas fa-external-link-alt"></i>
-                </a>
-              </button>
-            </div>
-          </div>
-          <div className="more-work">
-            <p>More</p>
-            <a href="https://github.com/codingisreallyhard" target="blank">
-              GitHub
-            </a>
-          </div>
+        <PortofolioImage />
+        <div className="more-work">
+          <p>More</p>
+          <a href="https://github.com/codingisreallyhard" target="blank">
+            GitHub
+          </a>
         </div>
       </div>
     </section>
