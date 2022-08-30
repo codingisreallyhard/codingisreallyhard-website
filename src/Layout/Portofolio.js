d9e522cee2be5c3d8a1e1546a3d0babe26679d89
codingisreallyhard <florinconstantin99@yahoo.com>
2022-08-30 23:30:46 +0300

    finished portofolio img code

--- src/Layout/Portofolio.js
+++ src/Layout/Portofolio.js
@@ -1,99 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Portofolio.css";
-import authapp from "../Assets/authapp.png";
-import guess from "../Assets/guessgame.png";
-import eshop from "../Assets/reacteshop.png";
-import { render } from "@testing-library/react";
 
+import PortofolioImage from "./PortofolioImage";
 function Portofolio() {
-  const [hover, onHover] = useState(<img src={eshop} alt="yeye"></img>);
-
-  function onMouseEnter() {
-    onHover(<img src={guess} alt="yeye"></img>);
-  }
-
-  function onMouseOut() {
-    onHover(<img src={eshop} alt="yeye"></img>);
-  }
   return (
     <section id="my-works">
       <div className="portfolio">
         <div className="proj-heading">
           <h1>Portfolio</h1>
         </div>
-        <div className="portfolio-content container">
-          <div className="proj-1">
-            <div className="pdiv">
-              {/* <img
-                src={hover}
-                onMouseEnter={() => {
-                  onHover(guess);
-                }}
-                onMouseOut={() => {
-                  onHover(eshop);
-                }}
-                alt="React Eshop project"
-                className="p1"
-              /> */}
-              {}
-            </div>
-            <div className="proj1-details">
-              <i className="fab fa-html5"></i>
-              <i className="fab fa-css3-alt"></i>
-              <i className="fab fa-js"></i>
-              <h2>React E-Shop</h2>
-              <p>Built Using HTML,CSS,React.js</p>
-              <button>
-                <a
-                  href="https://github.com/codingisreallyhard/React-E-Shop"
-                  target="blank"
-                >
-                  View source <i className="fas fa-external-link-alt"></i>
-                </a>
-              </button>
-            </div>
-          </div>
 
-          <div className="proj-2">
-            <img src={authapp} alt="React Auth" />
-            <div className="proj2-details">
-              <i className="fab fa-html5"></i>
-              <i className="fab fa-css3-alt"></i>
-              <h2>Small Autenthication App</h2>
-              <p>Built Using HTML,CSS, React.js</p>
-              <button>
-                <a
-                  href="https://github.com/codingisreallyhard/React-Auth-Project"
-                  target="blank"
-                >
-                  View source <i className="fas fa-external-link-alt"></i>
-                </a>
-              </button>
-            </div>
-          </div>
-          <div className="proj-2">
-            <img src={guess} alt="Guess game" />
-            <div className="proj2-details">
-              <i className="fab fa-html5"></i>
-              <i className="fab fa-css3-alt"></i>
-              <h2>Guess Game</h2>
-              <p>Built Using HTML,CSS, JAVASCRIPT</p>
-              <button>
-                <a
-                  href="https://github.com/codingisreallyhard/Guess-Game-Javascript"
-                  target="blank"
-                >
-                  View source <i className="fas fa-external-link-alt"></i>
-                </a>
-              </button>
-            </div>
-          </div>
-          <div className="more-work">
-            <p>More</p>
-            <a href="https://github.com/codingisreallyhard" target="blank">
-              GitHub
-            </a>
-          </div>
+        <PortofolioImage />
+        <div className="more-work">
+          <p>More</p>
+          <a href="https://github.com/codingisreallyhard" target="blank">
+            GitHub
+          </a>
         </div>
       </div>
     </section>
---
+++ src/Layout/PortofolioImage.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./PortofolioImage.css";
+
+function PortofolioImage() {
+  return (
+    <section className="maindivimg">
+      <div className="containerportofolioimg">
+        <div className="box">
+          <div className="imgBx">
+            <img
+              src={"https://i.ibb.co/T8T1S9t/reacteshop.png"}
+              className="portofolioimg"
+              alt="React E-shop"
+            />
+          </div>
+          <div class="content">
+            <div>
+              <h2>React E-Shop</h2>
+              <p>Built using HTML,CSS,REACT</p>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img
+              src="https://i.ibb.co/FzbJzfs/guessgame.png"
+              className="portofolioimg"
+              alt="Guess Game"
+            />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Guess Game</h2>
+              <p>Built using HTML,CSS,JAVASCRIPT</p>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img
+              src="https://i.ibb.co/wSRvBTR/authapp.png"
+              className="portofolioimg"
+              alt="Auth app"
+            />
+          </div>
+          <div class="content">
+            <div>
+              <h2>React Autenthication App</h2>
+              <p>Built using HTML,CSS,REACT</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PortofolioImage;
