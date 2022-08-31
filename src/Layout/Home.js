25bb7f9f2442348e84d18ef4419962cad204874b
codingisreallyhard <florinconstantin99@yahoo.com>
2022-08-31 11:53:16 +0300

    modified code in home.js

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <section className="home-content" id="home-page">
       <div className="name">
         <h1>Hi, I'm Florin</h1>
-        <p>A Web Developer in training.</p>
+        <p>A Frontend Developer in training.</p>
       </div>
     </section>
   );
