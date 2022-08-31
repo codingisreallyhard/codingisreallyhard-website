fe8fb88ae31821b10cb0ddb3823c0b5e7ed857bb
codingisreallyhard <florinconstantin99@yahoo.com>
2022-08-31 09:57:45 +0300

    added code

--- src/Layout/PortofolioImage.js
+++ src/Layout/PortofolioImage.js
@@ -9,41 +9,41 @@ function PortofolioImage() {
           <div className="imgBx">
             <img
               src={"https://i.ibb.co/T8T1S9t/reacteshop.png"}
-              className="portofolioimg"
+              className="portofolioimg "
               alt="React E-shop"
             />
           </div>
           <div class="content">
             <div>
               <h2>React E-Shop</h2>
-              <p>Built using HTML,CSS,REACT</p>
+              <p className="portofolio1">Built using HTML,CSS,REACT</p>
             </div>
           </div>
         </div>
-        <div class="box">
-          <div class="imgBx">
+        <div className="box">
+          <div className="imgBx">
             <img
               src="https://i.ibb.co/FzbJzfs/guessgame.png"
               className="portofolioimg"
               alt="Guess Game"
             />
           </div>
-          <div class="content">
+          <div className="content ">
             <div>
-              <h2>Guess Game</h2>
-              <p>Built using HTML,CSS,JAVASCRIPT</p>
+              <h2 className='portofolio2h2'>Guess Game</h2>
+              <p className="portofolio2">Built using HTML,CSS,JAVASCRIPT</p>
             </div>
           </div>
         </div>
-        <div class="box">
-          <div class="imgBx">
+        <div className="box">
+          <div className="imgBx">
             <img
               src="https://i.ibb.co/wSRvBTR/authapp.png"
               className="portofolioimg"
               alt="Auth app"
             />
           </div>
-          <div class="content">
+          <div className="content">
             <div>
               <h2>React Autenthication App</h2>
               <p>Built using HTML,CSS,REACT</p>
